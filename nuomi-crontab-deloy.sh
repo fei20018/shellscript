fc17c99ce07aac64263eaa991c3ac18a33f0db9d
fei.huang <fei_20018@163.com>
2013-07-12 12:59:29 +0800

    7-1

--- nuomi-crontab-deloy.sh
+++ nuomi-crontab-deloy.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
-echo "pls "
-read i
-echo $i
-echo dfksdjfksjdfl
-echo aaa
-read bbb
-=======
+echo -e "pls choose action"\
+         "\n[1] delpoy a new cron"\
+         "\n[2] updata a cron"\
+         "\n[3] delete a cron"
+read act
+case $act in
+1)
+	echo "you choose delpoy a new cron?[y/n]"
+        read yorn
+	echo $yorn
+	if [[ $yorn -eq y ]]
+	then
+	/bin/sh src/deploy.sh
+	fi	
+
+
+;;
+2)
+	echo "you choose updata a cron?[y/n]"
+        read yorn
+	echo $yorn;;
+3)
+	echo "you choose delete a cron?[y/n]"
+        read yorn
+	echo $yorn;;
+*)
+	echo "sorry!input erro!";;
+esac
+
